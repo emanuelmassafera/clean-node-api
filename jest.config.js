da609149fcaeaf633b23132c23df16d0f28dbf38
Emanuel Massafera <emanuel301@live.com>
2021-01-25 13:56:23 -0300

    chore: add jest configs for mongodb

--- jest.config.js
+++ jest.config.js
@@ -12,6 +12,9 @@ module.exports = {
   // The test environment that will be used for testing
   testEnvironment: 'node',
 
+  //
+  preset: '@shelf/jest-mongodb',
+
   // A map from regular expressions to paths to transformers
   transform: {
     '.+\\.ts$': 'ts-jest'
