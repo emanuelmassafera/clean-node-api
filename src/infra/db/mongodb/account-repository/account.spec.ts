6db36bf7a0c722b7c461f046999dab8e00ac56ee
Emanuel Massafera <emanuel301@live.com>
2021-01-28 15:19:25 -0300

    test: ensure AccountMongoRepository updates the account accessToken on updateAccessToken success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -57,18 +57,18 @@ describe('Account Mongo Repository', () => {
     expect(account).toBeFalsy()
   })
 
-  test('Should return an account on loadByEmail success', async () => {
+  test('Should update the account accessToken on updateAccessToken success', async () => {
     const sut = makeSut()
-    await accountCollection.insertOne({
+    const result = await accountCollection.insertOne({
       name: 'any_name',
       email: 'any_email@mail.com',
       password: 'any_password'
     })
-    const account = await sut.loadByEmail('any_email@mail.com')
+    const fakeAccount = result.ops[0]
+    expect(fakeAccount.accessToken).toBeFalsy()
+    await sut.updateAccessToken(fakeAccount._id, 'any_token')
+    const account = await accountCollection.findOne({ _id: fakeAccount._id })
     expect(account).toBeTruthy()
-    expect(account.id).toBeTruthy()
-    expect(account.name).toBe('any_name')
-    expect(account.email).toBe('any_email@mail.com')
-    expect(account.password).toBe('any_password')
+    expect(account.accessToken).toBe('any_token')
   })
 })
