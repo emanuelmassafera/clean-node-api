aec03465f6a8bd742f5c5eb2d4eec68fa6190a47
Emanuel Massafera <emanuel301@live.com>
2021-01-28 10:38:54 -0300

    test: ensure DbAuthentication throws if UpdateAccessTokenRepository throws

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -162,9 +162,9 @@ describe('DbAuthentication UseCase', () => {
     expect(updateSpy).toHaveBeenCalledWith('any_id', 'any_token')
   })
 
-  test('Should throw if TokenGenerator throws', async () => {
-    const { sut, tokenGeneratorStub } = makeSut()
-    jest.spyOn(tokenGeneratorStub, 'generate').mockReturnValueOnce(new Promise(
+  test('Should throw if UpdateAccessTokenRepository throws', async () => {
+    const { sut, updateAccessTokenRepositoryStub } = makeSut()
+    jest.spyOn(updateAccessTokenRepositoryStub, 'update').mockReturnValueOnce(new Promise(
       (resolve, reject) => reject(new Error()))
     )
     const promise = sut.auth(makeFakeAuthentication())
